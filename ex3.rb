3569e0599b7df6dbf4b14d31f57ae820393e1aa0
Yew31 <Ydbarker@gmail.com>
2016-12-14 17:36:58 +0000

    First three exercises

---
+++ ex3.rb
@@ -0,0 +1,33 @@
+# + plus is addition
+# - minus is subtraction
+# / slash is division
+# * asterisk is multiply
+# % percent is modulus - X divided by Y with J remainlng
+# < less-than is less than
+# > greater-than is more than
+# <= less-than-equal is ess than or equal to
+# >= greater-than-equal is more than or equal to
+
+puts "I will now count my chickens:"
+
+puts "Hens #{25.0 + 30.0 / 6.0}"
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
+
+puts "Now I will count the eggs:"
+
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+puts 3.0 + 2.0 < 5.0 - 7.0
+
+puts "What is 3 + 2? #{3.0 + 2.0}"
+puts "What is 5 - 7? #{5.0 - 7.0}"
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+puts "Is it greater? #{5.0 > -2.0}"
+puts "Is it greater or equal? #{5.0 >= -2.0}"
+puts "Is it less or equal? #{5.0 <= -2.0}"
