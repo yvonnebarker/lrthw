d3a7784cef114ce1c326ff1578fdf8c23e4f15d9
yvonnebarker <Ydbarker@gmail.com>
2016-12-20 15:23:25 +0000

    Exercise 18

---
+++ ex18.rb
@@ -0,0 +1,23 @@
+# Exercise 18: Names, Variables, Code, Functions
+
+def print_two(*args)
+  arg1, arg2 = args
+  puts "arg1: #{arg1}, arg2: #{arg2}"
+end
+
+def print_two_again(arg1, arg2)
+  puts "arg1: #{arg1}, arg2: #{arg2}"
+end
+
+def print_one(arg1)
+  puts "arg1: #{arg1}"
+end
+
+def print_none()
+  puts "I got nothin'."
+end
+
+print_two("Happy","Smiley")
+print_two_again("Happy","Smiley")
+print_one("First!")
+print_none()
