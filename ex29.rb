cad03522618297b1df82455ddba752b00119b18c
yvonnebarker <Ydbarker@gmail.com>
2017-01-05 16:33:22 +0000

    Exercise 29

---
+++ ex29.rb
@@ -0,0 +1,44 @@
+# Exercise 29: What If
+people = 20
+cats = 30
+dogs = 15
+
+
+if people < cats
+  puts "Too many cats! The world is doomed!"
+end
+
+if people > cats
+  puts "Not many cats! The world is saved!"
+end
+
+if people < dogs
+  puts "The world is drooled on!"
+end
+
+if people > dogs
+  puts "The world is dry!"
+end
+
+
+dogs += 8
+
+if people >= dogs
+  puts "People are greater than or equal to dogs."
+end
+
+if people <= dogs
+  puts "People are less than or equal to dogs."
+end
+
+if people == dogs
+  puts "People are dogs."
+end
+
+if ("cats" == "dogs" && 20 == 30)
+  puts "pets are not great"
+end
+
+if ("cats" == "cats" && 20 == 20)
+  puts "pets are great"
+end
