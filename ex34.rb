878b8fa4ec3deabc6f8f9768b7206352337942e9
yvonnebarker <Ydbarker@gmail.com>
2017-01-13 13:00:58 +0000

    Exercise 34

---
+++ ex34.rb
@@ -0,0 +1,18 @@
+#Exercise 34: Accessing Elements of Arrays
+animals = ["bear", "ruby", "peacock", "kangaroo", "whale", "platypus"]
+bear = animals[0]
+ruby = animals[1]
+peacock = animals[2]
+kangaroo = animals[3]
+whale = animals[4]
+platypus = animals[5]
+puts "The first (1st) animal is at 0 and is: #{animals[0]}"
+puts "The second (2nd) animal is at 1 and is: #{animals[1]}"
+puts "The three (3rd) animal is at 2 and is: #{animals[2]}"
+puts "The fourth (4th) animal is at 3 and is: #{animals[3]}"
+puts "The fifth (5th) animal is at 4 and is: #{animals[4]}"
+puts "The sixth (6th) animal is at 5 and is: #{animals[5]}"
+
+people = ["Charlie", "Lucy", "Helen", "Ada"]
+puts "In the exam #{people[0]} was top of the class, wheras #{people[3]} was bottom."
+puts "And #{people[-2]} came 3rd."
